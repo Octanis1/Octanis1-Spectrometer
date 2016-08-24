64cff8385f7da3d202dc39fb5ba559ee4656e524
Sam Sulaimanov <sam@bitmorse.com>
2016-08-24 17:04:06 +0200

    adding pngs of spectra

--- spectro.py
+++ spectro.py
@@ -10,12 +10,20 @@ This is a temporary script file.
 Created on Wed Nov 04 22:15:47 2015
 
 @author: Beat
+
+/dev/cu.usbmodem14123 for red launchpad on mac
 """
 
 import serial
-import matplotlib
+import matplotlib.pyplot as plt
+import time
+from IPython import display
+import pylab as pl
+import numpy as np
+
+
 ser = serial.Serial(
-    port=raw_input('Please enter serial port: '),
+    port="/dev/cu.usbmodem14123",
     baudrate=115200,
     parity=serial.PARITY_ODD,
     stopbits=serial.STOPBITS_ONE,
@@ -25,29 +33,55 @@ ser = serial.Serial(
 #ser.open()
 ser.isOpen()
 
-print 'Opened serial port'
 
-#get rid of incomplete dataset
-while ser.read(1) != '\n' :
-    pass
+while 1:
 
-#while 1 :
-charIn = ser.read(1)
-dataBuffer = charIn
-while charIn != '\n' :
-    charIn = ser.read(1)
-    dataBuffer += charIn
-
-myValuesString = dataBuffer.split(',')
-myValuesInt = []
-myValuesLength = 0
-for singleValue in myValuesString:
-    try:
-        myValuesInt.append(int(singleValue))
-        myValuesLength+=1
-    except:
-        pass
     
-matplotlib.pyplot.plot(range(myValuesLength), myValuesInt)
+    print 'Opened serial port' 
+    
+    #get rid of incomplete dataset
+    while ser.read(1) != '\n' :
+        pass
+    #while 1 :
+    charIn = ser.read(1)
+    dataBuffer = charIn
+    while charIn != '\n' :
+        charIn = ser.read(1)
+        dataBuffer += charIn
+    
+    myValuesString = dataBuffer.split(',')
+    myValuesInt = []
+    myValuesLength = 0
+  
+    for singleValue in myValuesString:
+        try:
+            myValuesInt.append(int(singleValue))
+           
+            myValuesLength+=1
+           
+            
+        except:
+            pass
+    
+    
+    print 'plotting'
+    display.clear_output(wait=True)
+    display.display(pl.gcf())
+    #myValuesInt.reverse()
+    
+    plt.minorticks_on()
+    plt.grid(color='r', linestyle='-', linewidth=1, which='both')
+    plt.plot(map(lambda x: x/float(2),range(myValuesLength)), myValuesInt)
+    
+    #plt.xticks(np.arange(0, 750, 0.5))    
+    
+    plt.axis([150,1200,60,200])
+    plt.show()
+    
+    time.sleep(1)
+    plt.close()
+    
+    
 
-ser.close();
+ser.close()
+    
